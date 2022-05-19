7cac06677da78f91754e77bfec1eab72aa02cf0e
Courtney Garcia <wavesofgrace3@gmail.com>
2022-05-19 15:25:11 -0700

    commit tree 1b57bce91e9132fc21e91cd2edbc76f5abbf3d02

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+# Class body
+    
+    # Instance Method Definintion
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end   
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+    # Class body
+
+    # instance method
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
